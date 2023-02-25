3888e60b2acceb18e2bd2b682b34d51b65a1df14
diwaneritesh@gmail.com <diwaneritesh@gmail.com>
2023-02-25 15:03:41 +0530

    removed postgresql URI

--- db.py
+++ db.py
@@ -25,27 +25,6 @@ def init_db():
     db.close()
 
 
-
-# def connect_db():
-#     conn = psycopg2.connect('postgres://bgiswqmjzwteqz:d72b5fbd07b542813789f62657471ea689bdf9fa1497703144c2b101c45bdd95@ec2-35-173-91-114.compute-1.amazonaws.com:5432/d7jmp1a2jp49a4',cursor_factory=DictCursor)
-#     conn.autocommit = True
-#     return conn
-
-# def get_db():
-#     db = connect_db()
-#     cur = db.cursor()
-#     if 'postgres_db_conn' not in g:
-#         g.postgres_db_conn = cur
-#     return g.postgres_db_conn
-
-# def init_db():
-#     db = connect_db()
-#     cur = db.cursor()
-#     cur.execute(open('schema.sql','r').read())
-#     cur.close()
-#     db.close()
-
-
 @click.command('init-db')
 def init_db_command():
     init_db()
